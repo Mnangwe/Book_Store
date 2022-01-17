22fdce7db41d2ed14ea0f166bf8f3e7747195db6
mnangwe <pupuma.azabenathi@gmail.com>
2022-01-17 09:54:33 +0200

    Error Handling

--- Assets/js/cart.js
+++ Assets/js/cart.js
@@ -12,20 +12,84 @@ function readBooks(items) {
           <div class="card-body">
             <h4 class="card-title">${product.title}</h4>
             <p><strong> ${product.title} by ${product.author} </strong>- </p>
-            <p class="card-text">$<strong> ${product.price}</strong></p>
+            <p class="card-text">$<strong> ${parseInt(product.price)}</strong></p>
             <div d-flex>
-            <input type="number" value=1 class="cart" min=1 id="addToCart${position}">
-            <button type="button" class="btn btn-secondary addToCart"  onclick="addToCart( ${position} )">
-              Add to Cart
-            </button>
+            <input type="number" value=${product.quantity} class="cart" min=1 id="addToCart${position}">
             </div>
+
             <div>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editBook${position}" >
-              Edit
-            </button>
-            <button type="button" class="btn btn-danger" onclick="deleteBook(${position})" >
-              Delete
-            </button>
-            </div>`
+            <button type="button" class="btn btn-primary" onclick="updateCart(${position})" >
+          Update
+        </button>
+        <button type="button" class="btn btn-danger" onclick="removeCart(${position})" >
+          Remove
+        </button>
+      </div>
+
+    </div>
+            `
     })
+
+    document.querySelector("#cost").innerHTML = `Total: R${ totalCost() }`
+}
+
+readBooks(cart)
+
+// UPDATE BOOK
+function updateCart(position) {
+  let quantity = document.querySelector(`#addToCart${position}`).value;
+  try {
+    if(quantity === cart[position].quantity) {throw new Error("You did not make any changes")}
+
+    cart[position] = ({...cart[position], quantity}) 
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    console.log(cart[position].quantity)
+    readBooks(cart)
+
+  } catch (error) {
+    alert(error)
+  }
+  
+  
+}
+
+
+// REMOVE BOOK
+function removeCart(position) {
+  let confirmation = confirm(
+    `Are you sure you want to remove ${cart[position].title}?`
+  );
+
+  if (confirmation) {
+    cart.splice(position, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    readBooks(cart);
+  }
+  totalCost();
+}
+
+// Total Cost
+function totalCost() {
+  let totalCost = 0
+    items = JSON.parse(localStorage.getItem("cart"))
+    for(let i = 0; i < items.length; i++){
+      
+      totalCost += items[i].price*items[i].quantity;
+
+    }
+    return parseInt(totalCost)
+}
+
+// Checkout
+function checkout(){
+  let confirmation = confirm(
+    `Your totalcost is ${totalCost()}`
+  );
+  if(confirmation){
+    cart.length = 0
+    localStorage.removeItem('cart')
+    readBooks(cart)
+  }
+
 }
--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,128 +1,128 @@
 let books = JSON.parse(localStorage.getItem("products"))
   ? JSON.parse(localStorage.getItem("products"))
   : [
-    { 
-        "image": "./Assets/images/AbbeyN.jpg",
-        "title": "Northanger Abbey",
-        "author": "Austen, Jane",
-        "year_written": 1814,
-        "edition": "Penguin",
-        "price": 18.2
-     }, 
-     {
-        "image":"./Assets/images/WarPeace.jpeg",
-        "title": "War and Peace",
-        "author": "Tolstoy, Leo",
-        "year_written": 1865,
-        "edition": "Penguin",
-        "price": 12.7
-     }, 
-     {
-        "image":"./Assets/images/AnnaK.jpeg",
-        "title": "Anna Karenina",
-        "author": "Tolstoy, Leo",
-        "year_written": 1875,
-        "edition": "Penguin",
-        "price": 13.5
-     }, 
-     {
-        "image":"./Assets/images/mrs-dalloway-162.jpg",
-        "title": "Mrs. Dalloway",
-        "author": "Woolf, Virginia",
-        "year_written": 1925,
-        "edition": "Harcourt Brace",
-        "price": 25
-     }, 
-     {
-        "image":"./Assets/images/hours.jpg",
-        "title": "The Hours",
-        "author": "Cunnningham, Michael",
-        "year_written": 1999,
-        "edition": "Harcourt Brace",
-        "price": 12.35
-     }, 
-     {
-        "image":"./Assets/images/FinnH.jpeg",
-        "title": "Huckleberry Finn",
-        "author": "Twain, Mark",
-        "year_written": 1865,
-        "edition": "Penguin",
-        "price": 5.76
-     }, 
-     {
-        "image":"./Assets/images/house.jpg",
-        "title": "Bleak House",
-        "author": "Dickens, Charles",
-        "year_written": 1870,
-        "edition": "Random House",
-        "price": 5.75
-     }, 
-     {
-        "image":"./Assets/images/TomS.jpg",
-        "title": "Tom Sawyer",
-        "author": "Twain, Mark",
-        "year_written": 1862,
-        "edition": "Random House",
-        "price": 7.75
-     }, 
-     {
-        "image":"./Assets/images/RoomOne.jpg",
-        "title": "A Room of One's Own",
-        "author": "Woolf, Virginia",
-        "year_written": 1922,
-        "edition": "Penguin",
-        "price": 29
-     }, 
-     {
-        "image":"./Assets/images/HarryPotter.jpeg",
-        "title": "Harry Potter",
-        "author": "Rowling, J.K.",
-        "year_written": 2000,
-        "edition": "Harcourt Brace",
-        "price": 19.95
-     }, 
-     {
-        "image":"./Assets/images/Hundred.jpeg",
-        "title": "One Hundred Years of Solitude",
-        "author": "Marquez",
-        "year_written": 1967,
-        "edition": "Harper Perennial",
-        "price": 14.00
-     }, 
-     {
-        "image":"./Assets/images/Hamlet.jpg",
-        "title": "Hamlet, Prince of Denmark",
-        "author": "Shakespeare",
-        "year_written": 1603,
-        "edition": "Signet Classics",
-        "price": 7.95
-     }, 
-     {
-        "image":"./Assets/images/LordRings.jpg",
-        "title": "Lord of the Rings",
-        "author": "Tolkien, J.R.",
-        "year_written": 1937,
-        "edition": "Penguin",
-        "price": 27.45
-     }
-  ]
+      {
+        image: "./Assets/images/AbbeyN.jpg",
+        title: "Northanger Abbey",
+        author: "Austen, Jane",
+        year: 1814,
+        edition: "Penguin",
+        price: 199.99,
+      },
+      {
+        image: "./Assets/images/WarPeace.jpeg",
+        title: "War and Peace",
+        author: "Tolstoy, Leo",
+        year: 1865,
+        edition: "Penguin",
+        price: 159.99,
+      },
+      {
+        image: "./Assets/images/AnnaK.jpeg",
+        title: "Anna Karenina",
+        author: "Tolstoy, Leo",
+        year: 1875,
+        edition: "Penguin",
+        price: 119.99,
+      },
+      {
+        image: "./Assets/images/mrs-dalloway-162.jpg",
+        title: "Mrs. Dalloway",
+        author: "Woolf, Virginia",
+        year: 1925,
+        edition: "Harcourt Brace",
+        price: 249.99,
+      },
+      {
+        image: "./Assets/images/hours.jpg",
+        title: "The Hours",
+        author: "Cunnningham, Michael",
+        year: 1999,
+        edition: "Harcourt Brace",
+        price: 220.00,
+      },
+      {
+        image: "./Assets/images/FinnH.jpeg",
+        title: "Huckleberry Finn",
+        author: "Twain, Mark",
+        year: 1865,
+        edition: "Penguin",
+        price: 70.00,
+      },
+      {
+        image: "./Assets/images/house.jpg",
+        title: "Bleak House",
+        author: "Dickens, Charles",
+        year: 1870,
+        edition: "Random House",
+        price: 70.00,
+      },
+      {
+        image: "./Assets/images/TomS.jpg",
+        title: "Tom Sawyer",
+        author: "Twain, Mark",
+        year: 1862,
+        edition: "Random House",
+        price: 85.00,
+      },
+      {
+        image: "./Assets/images/RoomOne.jpg",
+        title: "A Room of One's Own",
+        author: "Woolf, Virginia",
+        year: 1922,
+        edition: "Penguin",
+        price: 329.99,
+      },
+      {
+        image: "./Assets/images/HarryPotter.jpeg",
+        title: "Harry Potter",
+        author: "Rowling, J.K.",
+        year: 2000,
+        edition: "Harcourt Brace",
+        price: 260.00,
+      },
+      {
+        image: "./Assets/images/Hundred.jpeg",
+        title: "One Hundred Years of Solitude",
+        author: "Marquez",
+        year: 1967,
+        edition: "Harper Perennial",
+        price: 170.00,
+      },
+      {
+        image: "./Assets/images/Hamlet.jpg",
+        title: "Hamlet, Prince of Denmark",
+        author: "Shakespeare",
+        year: 1603,
+        edition: "Signet Classics",
+        price: 130.00,
+      },
+      {
+        image: "./Assets/images/LordRings.jpg",
+        title: "Lord of the Rings",
+        author: "Tolkien, J.R.",
+        year: 1937,
+        edition: "Penguin",
+        price: 404.99,
+      },
+    ];
 
 let cart = JSON.parse(localStorage.getItem("cart"))
-? JSON.parse(localStorage.getItem("cart"))
-: []
+  ? JSON.parse(localStorage.getItem("cart"))
+  : [];
 
 // READ BOOKS
 function readBooks(items) {
-    document.querySelector("#products").innerHTML = "";
-    items.forEach((product, position) => {
-      document.querySelector("#products").innerHTML += `
+  document.querySelector("#products").innerHTML = "";
+  items.forEach((product, position) => {
+    document.querySelector("#products").innerHTML += `
         
         <div class="card">
           <Image src="${product.image}" class="card-Image-top" alt="${product.title}">
           <div class="card-body">
             <h4 class="card-title">${product.title}</h4>
-            <p><strong> ${product.title} by ${product.author} </strong>- </p>
-            <p class="card-text">$<strong> ${product.price}</strong></p>
+            <p><strong> ${product.title} by ${product.author} </strong>- ${product.year} </p>
+            <p class="card-text">R<strong> ${parseInt(product.price)}</strong></p>
             <div d-flex>
             <input type="number" value=1 class="cart" min=1 id="addToCart${position}">
             <button type="button" class="btn btn-secondary addToCart"  onclick="addToCart( ${position} )">
@@ -137,7 +137,8 @@ function readBooks(items) {
               Delete
             </button>
             </div>
-             
+            
+            <!-- Modal Class -->
             <div
             class="modal fade"
             id="editBook${position}"
@@ -194,13 +195,13 @@ function readBooks(items) {
                     </select>
                   </div>
                   <div class="mb-3">
-                    <label for="editYear${position}" class="form-label">Year Written</label>
+                    <label for="editYear${position}" class="form-label">Year Published</label>
                     <input
                       class="form-control"
                       type="text"
                       name="editYear${position}"
                       id="editYear${position}"
-                      value="${product.price}"
+                      value="${product.year}"
                     />
                   </div>
                   <div class="mb-3">
@@ -210,7 +211,7 @@ function readBooks(items) {
                       type="text"
                       name="editPrice${position}"
                       id="editPrice${position}"
-                      value="${product.price}"
+                      value="${parseInt(product.price)}"
                     />
                   </div>
                   <div class="mb-3">
@@ -220,7 +221,7 @@ function readBooks(items) {
                       type="text"
                       name="editImage${position}"
                       id="editImage${position}"
-                      value="${product.Image}"
+                      value="${product.image}"
                     />
                   </div>
                 </div>
@@ -248,84 +249,82 @@ function readBooks(items) {
   </div>
   `;
   });
-  }
-  
-  readBooks(books);
+}
 
+readBooks(books);
 
 // CREATE a BOOK
 function createBook() {
-    let title = document.querySelector("#addTitle").value;
-    let edition = document.querySelector("#addEdition").value;
-    let price = document.querySelector("#addPrice").value;
-    let image = document.querySelector("#addImage").value;
-    let year = document.querySelector("#addYear").value;
-    let author = document.querySelector("#addAuthor").value;
-  
-    try {
-      if (!title || !price || !image) throw new Error("Please fill in all fields");
-      books.push({
-        title,
-        edition,
-        price,
-        image,
-        year,
-        author
-      });
-      localStorage.setItem("products", JSON.stringify(books));
-      readBooks(books);
-    } catch (err) {
-      alert(err);
-      console.log(err)
-    }
+  let title = document.querySelector("#addTitle").value;
+  let edition = document.querySelector("#addEdition").value;
+  let price = parseInt(document.querySelector("#addPrice").value);
+  let image = document.querySelector("#addImage").value;
+  let year = document.querySelector("#addYear").value;
+  let author = document.querySelector("#addAuthor").value;
+
+  try {
+    if (!title || !price || !image)
+      throw new Error("Please fill in all fields");
+    books.push({
+      title,
+      edition,
+      price,
+      image,
+      year,
+      author,
+    });
+    localStorage.setItem("products", JSON.stringify(books));
+    readBooks(books);
+  } catch (err) {
+    alert(err);
+    console.log(err);
   }
+}
 // UPDATE A BOOK
-  function updateBook(position) {
-    let title = document.querySelector(`#editTitle${position}`).value;
-    let edition = document.querySelector(`#editEdition${position}`).value;
-    let price = document.querySelector(`#editPrice${position}`).value;
-    let image = document.querySelector(`#editImage${position}`).value;
-    let year = document.querySelector(`#editYear${position}`).value;
-    let author = document.querySelector(`#editAuthor${position}`).value;
-  
-    try {
-      if (!title || !price || !image) throw new Error("Please fill in all fields");
-      books[position] = {
-        title,
-        edition,
-        price,
-        image,
-        year,
-        author
-      };
-      localStorage.setItem("products", JSON.stringify(books));
-      readBooks(books);
-    } catch (err) {
-      alert(err);
-    }
+function updateBook(position) {
+  let title = document.querySelector(`#editTitle${position}`).value;
+  let edition = document.querySelector(`#editEdition${position}`).value;
+  let price = parseInt(document.querySelector(`#editPrice${position}`).value);
+  let image = document.querySelector(`#editImage${position}`).value;
+  let year = document.querySelector(`#editYear${position}`).value;
+  let author = document.querySelector(`#editAuthor${position}`).value;
+
+  try {
+    if (!title || !price || !image)
+      throw new Error("Please fill in all fields");
+    books[position] = {
+      title,
+      edition,
+      price,
+      image,
+      year,
+      author,
+    };
+    localStorage.setItem("products", JSON.stringify(books));
+    readBooks(books);
+  } catch (err) {
+    alert(err);
   }
+}
 
 // DELETE A BOOK
 function deleteBook(position) {
-    let confirmation = confirm(
-      "Are you sure you want to delete the selected product?"
-    );
-  
-    if (confirmation) {
-      books.splice(position, 1);
-      localStorage.setItem("products", JSON.stringify(books));
-      readBooks(books);
-    }
+  let confirmation = confirm(
+    `Are you sure you want to remove ${books[position].title.toUpperCase()} from the list?`
+  );
+
+  if (confirmation) {
+    books.splice(position, 1);
+    localStorage.setItem("products", JSON.stringify(books));
+    readBooks(books);
   }
-  
-
-
+}
 
 // ADD TO CART
-function addToCart(position){
-    let quantity = document.querySelector(`#addToCart${position}`).value
-    cart.push({quantity, ...books[position]})
-    // cart.push({...products[position]});
-    localStorage.setItem('cart', JSON.stringify(cart))
-    console.log(cart)
-  }
+function addToCart(position) {
+  let quantity = document.querySelector(`#addToCart${position}`).value;
+  cart.push({ quantity, ...books[position] });
+  // cart.push({...products[position]});
+  localStorage.setItem("cart", JSON.stringify(cart));
+  console.log(cart);
+}
